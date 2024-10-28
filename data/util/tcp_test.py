c863adfcad57999f147c039dc83f1800a0f14d6a
gnij <1006631645@qq.com>
2024-10-28 10:10:10 +0800

    10.28

--- data/util/tcp_test.py
+++ data/util/tcp_test.py
@@ -7,8 +7,13 @@ def tcp_test():
     server_ip = '127.0.0.1'
     server_port = 8051
     client_socket.connect((server_ip, server_port))
-    while True:
-        client_socket.send("12345".encode('utf-8'))
+    try:
+        while True:
+            time1=time.strftime('%Y%m%d%H%M%S')
+            client_socket.send(time1.encode('utf-8'))  # 发送数据
+            time.sleep(0.01)  # 增加发送间隔
+    finally:
+        client_socket.close()  # 确保关闭连接
 
 
 
@@ -26,3 +31,5 @@ def tcp_calc():
 
     # 设置接收缓冲区大小为 256KB
 tcp_test()
+
+
